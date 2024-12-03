818659adf65c9d8a6881e3bf40163757be8780b1
Tom Tiedemann <tomtiedemann30@gmail.com>
2024-12-04 00:51:40 +0100

    als het goed is klaar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
+const clockFps = 60;
+
 window.onload = function() {
     clockInitialize();
+    fetchBoardData();
 
-    setInterval(clockInitialize, 1000);
+    setInterval(fetchBoardData, 15000);
+    setInterval(clockInitialize, 1000 / clockFps);
 };
+
 function clockInitialize(){
     var date = new Date();
-    var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    var time = [date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds()];
     var clockDiv = [document.getElementById("hour"), document.getElementById("minute"), document.getElementById("second")];
     
     var hour = time[1]/2+time[0]*30;
+    var minute = time[1] * 6;
+    var second = (time[2] + time[3] / 1000) * 6;
     
     clockDiv[0].style.transform="rotate("+ hour +"deg)";
-    clockDiv[1].style.transform="rotate("+ time[1]*6 +"deg)";
-    clockDiv[2].style.transform="rotate("+ time[2]*6 +"deg)";
+    clockDiv[1].style.transform="rotate("+ minute +"deg)";
+    clockDiv[2].style.transform="rotate("+ second +"deg)";
 }
 
-const fetchBoardData = async () => {
+async function fetchBoardData() {
     const apiKey = '2ebfd784a79a4470be6818c17d8ff046';
     const stationCode = 'UT'; 
     const spoor = '18'; 
@@ -38,16 +45,45 @@ const fetchBoardData = async () => {
             return;
         }
 
-        const departuresFromSpoor = data.payload.departures.filter(
-            (departure) => departure.plannedTrack === spoor || departure.actualTrack === spoor
-        );
+        //const departuresFromSpoor = data.payload.departures.filter(
+        //    (departure) => departure.plannedTrack === spoor || departure.actualTrack === spoor
+        //);
 
-        if (departuresFromSpoor.length === 0) {
-            console.log(`Geen vertrekkende treinen van spoor ${spoor}.`);
-            return;
+        //const nextTwoTrains = departuresFromSpoor.slice(0, 2);
+        const nextTwoTrains = data.payload.departures.slice(0, 2);
+
+        if(nextTwoTrains[0]){
+            var departureTime = new Date(nextTwoTrains[0].plannedDateTime);
+            var departureMinutes = Math.floor((departureTime - new Date()) / 60000);
+
+            var routeStationsString = '';
+            nextTwoTrains[0].routeStations.forEach((station, index) => {
+                if(index === 0){
+                    routeStationsString += station.mediumName;
+                }else{
+                    routeStationsString += ', ' + station.mediumName;
+                }
+            });
+
+            if(nextTwoTrains[0].routeStations.length == 0){
+                routeStationsString = nextTwoTrains[0].direction;
+            }else if(nextTwoTrains[0].routeStations.length >= 1){
+                routeStationsString += ' en ' + nextTwoTrains[0].direction;
+            }
+
+            document.querySelector('#middle #board #categoryName').innerHTML = nextTwoTrains[0].product.longCategoryName;
+            document.querySelector('#middle #board #departureMinutes').innerHTML = `<span>${departureMinutes}</span> ${departureMinutes === 1 ? 'minuut' : 'minuten'}`;
+            document.querySelector('#middle #board #direction').innerHTML = nextTwoTrains[0].direction;
+            document.querySelector('#middle #board #routeStations span').innerHTML  = routeStationsString;
         }
 
-        const nextTwoTrains = departuresFromSpoor.slice(0, 2);
+        if(nextTwoTrains[1]){
+            var departureTime = new Date(nextTwoTrains[1].plannedDateTime);
+
+            document.querySelector('#middle #bottom p span').innerHTML = `${departureTime.getHours()}:${departureTime.getMinutes()} ${nextTwoTrains[1].trainCategory} ${nextTwoTrains[1].direction}`;
+        }else{
+            document.querySelector('#middle #bottom p').style.display = 'none';
+        }
 
         console.log(nextTwoTrains);
     } catch (error) {
